243940aafd7deccfffe4be8e17071e8bdb605660
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2015-11-23 21:24:06 +0700

    insert input and call cover service

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 var roman = angular.module("romanApp", []);
 
-roman.controller("romanController", [function() {
+roman.controller("romanController", ["convertService", function(convertService) {
   var ctrl = this;
   ctrl.test = "aaabbb";
+  ctrl.input = "";
+  ctrl.result = "";
+
+  ctrl.handleChange = function() {
+    ctrl.result = convertService.toRoman(ctrl.input);
+  };
+}]);
+
+roman.factory("convertService", [function() {
+  var service = this;
+  service.toRoman = function(input) {
+    return "ffgggs";
+  };
+  return service;
 }]);
