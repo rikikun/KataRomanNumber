957f9cedd8a350d62e9dd00c4975924fef11c6b6
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2015-11-30 08:12:18 +0700

    validate input before enter form

--- src/app.js
+++ src/app.js
@@ -5,32 +5,34 @@ roman.controller("romanController", ["convertService", function(convertService) 
   ctrl.test = "aaabbb";
   ctrl.input = "";
   ctrl.result = "";
+  ctrl.error = "";
 
-  ctrl.handleChange = function() {
-    ctrl.result = convertService.toRoman(ctrl.input.trim());
+  ctrl.handleChange = function(event) {
+    if(!isNaN(ctrl.input)) {
+      ctrl.error = "";
+      ctrl.result = convertService.toRoman(ctrl.input.trim());
+    }else {
+      ctrl.error = "Should be only 1-1000";
+    }
   };
+
+  ctrl.handleKeypress = function(event) {
+    if(isNaN(String.fromCharCode(event.charCode))) {
+      ctrl.error = "Should be only 1-1000";
+      event.preventDefault();
+    }else {
+      ctrl.error = "";
+    }
+  };
+
 }]);
 
 roman.factory("convertService", [function() {
   var service = this;
   service.toRoman = function(input) {
-
-
     input = Number(input);
     var maps = {
-      M:1000,
-      CM:900,
-      D:500,
-      CD:400,
-      C:100,
-      XC:90,
-      L:50,
-      XL:40,
-      X:10,
-      IX:9,
-      V:5,
-      IV:4,
-      I:1
+      M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1
     };
     var result = '';
     var index;
@@ -42,7 +44,6 @@ roman.factory("convertService", [function() {
     }
     return result;
   };
-
 
   return service;
 }]);
